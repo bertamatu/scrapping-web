8ae85864a655e4f4d19f7171150fa82c17400136
bertamatu <info.berta@gmail.com>
2020-07-22 18:04:32 +0300

    scrapping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
+const axios = require("axios");
+const cheerio = require("cheerio");
+const BASE_URL = `http://www.lrimones.lt/index.php?imonespaieska=&raktazodis=`;
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    getSearchResults();
+  }, []);
+
+  async function getSearchResults(searchQuery) {
+    setLoading(true);
+    const response = await axios.get(`${BASE_URL}${searchQuery}`);
+    setLoading(false);
+    const data = response.data;
+    const $ = cheerio.load(data);
+    $(".contact").map((i, e) => {
+      const CompanyInfo = $(e).text();
+      console.log(CompanyInfo);
+    });
+    // ---- HOW TO setCompanies ???
+  }
+
+  if (loading) {
+    return (
+      <p style={{ margin: 50, fontWeight: "bold", color: "green" }}>
+        loading..
+      </p>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        style={{ margin: 50, padding: 10 }}
+        type="text"
+        placeholder="Enter text..."
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+      />
+      <button onClick={() => getSearchResults(searchQuery)}>Search</button>
+      <br />
+      {/* <ul>
+        {companies.map((company, index) => (
+          <li key={index}>{company}</li>
+        ))}
+      </ul> */}
     </div>
   );
 }
